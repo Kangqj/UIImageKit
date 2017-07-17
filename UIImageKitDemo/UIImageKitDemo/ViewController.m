67fa100285e41b52def5a5c45c07ab22dbf196d7
Your Name <you@example.com>
2017-07-17 23:02:47 +0800

    Release 1.0.0.

---
+++ UIImageKitDemo/UIImageKitDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UIImageKitDemo
+//
+//  Created by 康起军 on 17/7/17.
+//  Copyright © 2017年 康起军. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "UIColor+CustomExtensions.h"
+#import "UIImage+Generate.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
